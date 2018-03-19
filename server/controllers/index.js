47128aae49770a8db559a9f055d35b3b668a06c7
Elvis Perez <elvisalexiperez@gmail.com>
2018-03-19 07:15:25 -0400

    DomoMaker A complete

---
+++ server/controllers/index.js
@@ -0,0 +1,4 @@
+// this file gets imported when the folder is imported
+// pull other files we want into public scope
+module.exports.Account = require('./Account.js');
+module.exports.Domo = require('./Domo.js');
